0213be0e0077d353949564662f9396c2c44ec20e
22aishwaryagoyal <69304484+22aishwaryagoyal@users.noreply.github.com>
2021-08-10 18:56:35 +0530

    Add files via upload

---
+++ organizational-service/src/main/java/com/fisglobal/organizationalservice/model/Organizational.java
@@ -0,0 +1,26 @@
+package com.fisglobal.organizationalservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="org1")
+public class Organizational
+{
+	@Id
+	private long orgId;
+	@Size(min=2,max=15,message="Name should contain atleast 15 chars")
+	private String orgName;
+	@NotNull
+	private String loc;
+}
